0c8d3216d3fcf2fa76f429a9157883ee65c77658
yslandrei <andreidiaconescu2003@gmail.com>
2024-02-19 21:17:40 +0200

    first commit

---
+++ src/main/java/com/example/nutribackend/NutriBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutribackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nutribackend/NutriBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutribackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
